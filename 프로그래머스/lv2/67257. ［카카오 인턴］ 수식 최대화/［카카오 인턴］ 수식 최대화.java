319378b6dd86adededc56bb35d52f88465b9474e
Jaehee Park <54929552+pjh4400@users.noreply.github.com>
2022-05-12 17:27:21 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.54 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,11 +1,12 @@
 import java.util.*;
 
 class Solution {
-
+    
     List<Long> numbers = new ArrayList<>();
     List<Character> operators = new ArrayList<>();
-    Set<Character> operatorSet = new HashSet<>(); // 사용된 연산자 세트
+    Set<Character> operatorSet = new HashSet<>();
     long answer = 0;
+
 
     public long solution(String expression) {
         StringBuilder sb = new StringBuilder();
@@ -19,49 +20,48 @@ class Solution {
                 operators.add(expression.charAt(i));
             }
         }
-        numbers.add(Long.parseLong(sb.toString())); // 마지막 숫자 넣어준다.
-        sb.setLength(0);
-        List<Character> operatorList = new ArrayList<>(operatorSet); // 순서가 지켜져야하므로 List 로 변환한다.
+        List<Character> operatorList = new ArrayList<>(operatorSet);
         boolean[] visited = new boolean[operatorList.size()];
         makePriority(operatorList, new LinkedList<>(), visited);
-        return answer;
+        return answer; // 절댓값
     }
 
-    // 연산자 우선순위 순열 구하기
-    void makePriority(List<Character> operatorList, List<Character> priority, boolean[] visited) {
-        if (visited.length == priority.size()) {
+    // 연산자 우선순위
+    void makePriority(List<Character> operatorList, Queue<Character> priority, boolean[] visited) {
+        if (operatorList.size() == priority.size()) {
             answer = Math.max(answer, calculate(priority));
             return;
         }
 
         for (int i = 0; i < operatorList.size(); i++) {
             if (!visited[i]) {
-                visited[i] = true;
                 priority.add(operatorList.get(i));
                 makePriority(operatorList, priority, visited);
-                priority.remove(priority.size()-1);
                 visited[i] = false;
             }
         }
     }
 
     // 계산
-    Long calculate(List<Character> priority) {
+    Long calculate(Queue<Character> priority) {
         List<Character> copyOperators = new ArrayList<>(List.copyOf(operators));
         List<Long> copyNumbers = new ArrayList<>(List.copyOf(numbers));
-        for(char currentOperator : priority) {
+
+        while (!priority.isEmpty()) {
+            char currentOperator = priority.poll();
             for (int i = 0; i < copyOperators.size(); i++) {
                 if (copyOperators.get(i) != currentOperator) {
                     continue;
                 }
                 long result = getCalculateResult(copyNumbers.get(i), copyNumbers.get(i + 1), currentOperator);
                 copyNumbers.remove(i + 1); // 계산 완료 된 뒤 피연산자 제거
-                copyNumbers.set(i, result); // 계산 완료 된 앞 피연산자를 계산 결과로 대체
+                copyNumbers.remove(i); // 계산 완료 된 앞 피연산자를 계산 결과로 대체
+                copyNumbers.add(i, result);
                 copyOperators.remove(i); // 계산 완료 후 연산자 제거
-                i--; // 연산자 줄었으므로 인덱스도 줄여준다.
+                i--; // 연산자 삭제
             }
         }
-        return Math.abs(copyNumbers.get(0)); // 절댓값
+        return Math.abs(copyNumbers.get(0));
     }
 
     long getCalculateResult(long a, long b, char currentOperator) {
