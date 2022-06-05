8914c587bcab86f8ff1c5244fcc21bbb23e4c523
Jaehee Park <54929552+pjh4400@users.noreply.github.com>
2022-06-05 17:54:35 +0900

    [level 2] Title: [3차] n진수 게임, Time: 1.80 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder answer = new StringBuilder();
+        StringBuilder numberStr = new StringBuilder();
+        int length = m * t; // 필요한 숫자 수
+        for (int i = 0; numberStr.length() <= length; i++) {
+            numberStr.append(Integer.toString(i, n));
+        }
+        // 튜브의 순서에 해당하는 글자들만 추출
+        for (int i = p - 1; answer.length() < t; i += m) {
+            answer.append(numberStr.charAt(i));
+        }
+        return answer.toString().toUpperCase();
+    }
+}
